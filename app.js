1dec36c308e031b5f95098c4842d6b1e6b289bd6
RipSNake <seba.guzman13@gmail.com>
2021-01-14 09:55:07 -0300

    Add passport passport-local passport-local-mongoose. Configure passport and session. Connect to database

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const logger = require('morgan');
+const passport = require('passport');
+const User = require('./models/user');
+const session = require('express-session');
+const mongoose = require('mongoose');
 
+// require routes
 const indexRouter = require('./routes/index');
 const postsRouter = require('./routes/posts');
 const reviewsRouter = require('./routes/reviews');
@@ -11,6 +17,14 @@ const reviewsRouter = require('./routes/reviews');
 
 const app = express();
 
+// Connect to the database
+mongoose.connect('mongodb://localhost:27017/surf-shop', { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection errors:'));
+db.once('open', () => {
+  console.log('we\'re connected');
+})
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,6 +35,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Set up session
+app.use(session({
+  secret: 'overloaded mind',
+  resave: false,
+  saveUninitialized: true,
+  // cookie: { secure: true }
+}))
+
+// Set up passport
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Routes
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
 app.use('/posts/:id/reviews', reviewsRouter); // to have access to the id of the post the review belongs to
