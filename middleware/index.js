96f4f4688ad2bbd1518f1df628876560b1eec7e4
RipSNake <seba.guzman13@gmail.com>
2021-01-15 10:20:09 -0300

    Add notes on redirection

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,8 @@
 module.exports = {
 	errorHandler: (fn) => {
 		(req, res, next) => {
-			Promise.resolve(fn(req, res, next))
-					.catch(next);
+			Promise.resolve(fn(req, res, next)) // If successfull redirects to index page (controllers/index.js)
+					.catch(next); // If failure redirects to error message
 		}
 	}
 }
