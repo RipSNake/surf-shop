e432161a23bf48d921ec43a9df61cad8983ecef2
RipSNake <seba.guzman13@gmail.com>
2021-01-25 09:42:43 -0300

    create asyncErrorHandler

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,8 @@
 module.exports = {
-	errorHandler: (fn) => {
+	asyncErrorHandler: (fn) => 
 		(req, res, next) => {
 			Promise.resolve(fn(req, res, next)) // If successfull redirects to index page (controllers/index.js)
 					.catch(next); // If failure redirects to error message
 		}
-	}
+	
 }
