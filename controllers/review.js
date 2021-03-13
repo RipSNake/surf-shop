e29e5721872cb2e52854f4b8fae475f35fc809d5
RipSNake <seba.guzman13@gmail.com>
2021-03-13 14:34:09 -0300

    add review population for post show method

--- controllers/review.js
+++ controllers/review.js
@@ -4,18 +4,19 @@ const Review = require('../models/review');
 module.exports = {
 	// Create Review
 	async reviewCreate(req, res, next) {
-
+		// find the post by its id
+		let post = await Post.findById(req.params.id);
+		// create the review
+		req.body.review.author = req.user._id;
+		let review = await Review.create(req.body.review);
+		// assign review to post
+		post.reiews.push(review);
+		// save the post
+		post.save();
+		// redirect to the post
+		req.session.success = 'Review Created Successfully!';
+		res.redirect(`/posts/${post.id}`);
 	},
-	/*
-	// Show Review
-	async reviewShow(req, res, next) {
-		// we won't use show 'cause we'll show the reviews inside the post	
-	},
-	// Edit Review
-	async editReview(req, res, next) {
-		// we won't use show 'cause we'll show the reviews inside the post
-	},
-	*/
 	// Update Review
 	async reviewUpdate(req, res, next) {
 
